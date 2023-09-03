e0e89f53e8c5987a9825b282a7cafe0af09563e1
Aker <@akerke.n@inbox.ru>
2023-09-04 00:24:34 +0600

    fetchData

--- univercity/ViewController.swift
+++ univercity/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import Alamofire
+import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
+    
+    var universities: [University] = []
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      setupScene()
+        makeConstraints()
+       
     }
 
 
 }
+
+private extension ViewController {
+    func setupScene() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func makeConstraints() {
+        tableView.snp.makeConstraints{
+            $0.edges.equalToSuperview()
+        }
+        func fetchData() {
+            
+            let url = "http://universities.hipolabs.com/search?country=United+States"
+            
+            Alamofire.request(url, method: .get).responseJSON { response in
+                switch response.result {
+                case .success(let data):
+                    if let universitiesData = data as? [[String: Any]] {
+                        self.universities = universitiesData.compactMap { universityData in
+                            return University(name: universityData["name"] as? String ?? "",
+                                              country: universityData["country"] as? String ?? "")
+                        }
+                        self.tableView.reloadData()
+                    }
+                case .failure(let error):
+                    print("Error fetching data: \(error)")
+                }
+            }
+        }
+    }
+    }
+    
+    extension ViewController: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return universities.count
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "UniversityCell", for: indexPath) as! UniversityTableViewCell
+            let university = universities[indexPath.row]
+            cell.nameLabel.text = university.name
+            cell.countryLabel.text = university.country
+            return cell
+        }
+        
+        
+    }
 
---
+++ univercity/model.swift
@@ -0,0 +1,13 @@
+//
+//  model.swift
+//  univercity
+//
+//  Created by Akerke on 02.09.2023.
+//
+
+import Foundation
+
+struct University {
+    let name: String
+    let country: String
+}
